f89360d598eae83cb535e148573024332f90b960
Carlos André Ferrari <caferrari@gmail.com>
2024-05-17 14:40:13 -0300

    php 8.3

--- build-images.sh
+++ build-images.sh
@@ -1,21 +1,21 @@
-VERSIONS="8.1"
+VERSIONS="8.2 8.3"
 VARIATIONS="cli fpm"
 TAGS="cli fpm cli-build"
 
 for version in $VERSIONS; do
     for variation in $VARIATIONS; do
-        docker pull "php:$version-$variation-buster"
+        docker pull "php:$version-$variation-bookworm"
     done
 done
 
 for version in $VERSIONS; do
     for tag in $TAGS; do
-        docker build -t "eduzz/php:$version-$tag" -f "$version-$tag" .
+        docker build -t "eduzz/php:$version-$tag-beta" -f "$version-$tag" .
     done
 done
 
 for version in $VERSIONS; do
     for tag in $TAGS; do
-        docker push "eduzz/php:$version-$tag"
+        docker push "eduzz/php:$version-$tag-beta"
     done
 done
--- composer.sh
+++ composer.sh
@@ -1,7 +1,7 @@
 set -x \
     && apt-get update \
     && apt-get install -y zip unzip git \
-    && pecl install xdebug-3.2.0 \
+    && pecl install xdebug-3.3.2 \
     && docker-php-ext-enable xdebug \
     && php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
     && php composer-setup.php --install-dir=/usr/local/bin \
